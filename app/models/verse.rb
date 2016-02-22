c0aee2de7879cfba46a7a8fde42b1364d7eb2365
Joshua Schultz <me@joshuaschultz.com>
2016-02-21 19:41:24 -0600

    cleaned up navigation and user flow

--- app/models/verse.rb
+++ app/models/verse.rb
@@ -2,4 +2,10 @@ class Verse < ActiveRecord::Base
   belongs_to :chapter
   has_many :translations
   accepts_nested_attributes_for :translations
+
+  def topverse
+    translations.order(upvotes: 'desc').first
+  end
+
+
 end
